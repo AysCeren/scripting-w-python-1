12689744c28d2fe54f6aa54bd34ad7e02aa87232
AysCeren <cerenaysebulut@gmail.com>
2025-08-13 16:07:49 +0300

    finished

--- get_game_data.py
+++ get_game_data.py
@@ -4,7 +4,84 @@ import shutil
 from subprocess import run, PIPE
 import sys
 
+GAME_DIR_PATTERN = "game"
+GAME_CODE_EXTENSION = ".go"
+GAME_COMPILE_COMMAND = ["go","build"]
+def find_all_game_dirs(source):
+    game_paths = []
+    for root, dirs, files in os.walk(source):
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                game_paths.append(path)
+        break
+    return game_paths
+
+def get_name_from_path(paths,to_strip):
+    new_names = []
+    for path in paths:
+        print("Processing path:", path)
+        _, dir_name = os.path.split(path) #C:\scripting will be eliminated
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+
+    return new_names
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def copy_and_overwrite(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    shutil.copytree(source, destination)
+def make_json_metadata_file(path, game_dirs):
+    data = {
+    "gameName" : game_dirs,
+    "numberOfGames" : len(game_dirs)
+    }
+    with open(path, "w") as f:
+        json.dump(data,f) #if with is not used, the file will not be closed automatically
+def compile_game_code(path):
+    code_file_name = None
+    for root, dirs,files in os.walk(path):
+        for file in files:
+            if file.endswith(GAME_CODE_EXTENSION):
+                code_file_name = file
+        break
+    if code_file_name is None:
+        return
+    
+    command = GAME_COMPILE_COMMAND + [code_file_name]
+def run_command(command, path):
+    cwd = os.getcwd()
+    os.chdir(path)
+    #it will run the command in the path
+    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    print("compile result:", result)
+    os.chdir(cwd) #change back to the original directory
+def main(source, target):
+    cwd = os.getcwd() #C:\scripting
+    source_path = os.path.join(cwd, source) #C:\scripting\data 
+    target_path = os.path.join(cwd, target) #C:\scripting\target
+    game_paths = find_all_game_dirs(source_path)
+    new_game_dirs = get_name_from_path(game_paths, "_game")
+    
+    create_dir(target_path)
+
+    for src, dest in zip(game_paths, new_game_dirs):
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
+        compile_game_code(dest_path)
+
+    json_path = os.path.join(target_path, "metadata.json")
+    make_json_metadata_file(json_path, new_game_dirs)
+
 if __name__ == "__main__":
     args = sys.argv
     if(len(args)!=3):
         raise Exception("You must pass a source and target directory -only.")
+
+    source, target = args[1:] #the first argument is the script name, which we ignore
+    main (source, target)
